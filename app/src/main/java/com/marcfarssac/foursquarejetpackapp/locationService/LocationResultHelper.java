5c0215ebf0ed9751667918c2889911de88396bd4
Marc Farssac <marc.farssac@gmail.com>
2018-10-05 22:21:59 +0200

    Location Services code clean up

--- app/src/main/java/com/marcfarssac/foursquarejetpackapp/locationService/LocationResultHelper.java
+++ app/src/main/java/com/marcfarssac/foursquarejetpackapp/locationService/LocationResultHelper.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.marcfarssac.foursquarejetpackapp.locationService;
 
 import android.content.Context;
@@ -82,7 +66,7 @@ public class LocationResultHelper {
     /**
      * Fetches location results from {@link android.content.SharedPreferences}.
      */
-    public static String getSavedLon(Context context) {
+    private static String getSavedLon(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(KEY_LOCATION_LON, YOUR_MD_LON);
     }
@@ -90,7 +74,7 @@ public class LocationResultHelper {
     /**
      * Fetches location results from {@link android.content.SharedPreferences}.
      */
-    public static String getSavedLat(Context context) {
+    private static String getSavedLat(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(KEY_LOCATION_LAT, YOUR_MD_LAT);
     }
