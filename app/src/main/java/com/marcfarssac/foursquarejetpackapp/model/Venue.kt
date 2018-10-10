e62c11db72cf29d5c933a528e145abc3989d5310
Marc Farssac <marc.farssac@gmail.com>
2018-10-10 09:11:13 +0200

    Update Entity, DAO and Db version to store exact results for a given query (with accepted advantages and drawbacks)
    Changed database version to 2
    
    The Foursquare API algorithm returns Venues not only related to the Venue names keyed in. For this reason it is not possible to search locally for the words in the Venue name. Local queries shall be read using the same query. This means that similar queries will not work offline at this stage, but old ones will produce locally the same results than the ones given by the backend.
    
    Cached results are only meant for offline work. Even if they are present and there is a network connection a new call will be made and the response cached for future offline use.

--- app/src/main/java/com/marcfarssac/foursquarejetpackapp/model/Venue.kt
+++ app/src/main/java/com/marcfarssac/foursquarejetpackapp/model/Venue.kt
@@ -1,6 +1,7 @@
 package com.marcfarssac.foursquarejetpackapp.model
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
@@ -11,13 +12,40 @@ import com.google.gson.annotations.SerializedName
  * with the serialized name.
  * This class also defines the Room repos table, where the repo [id] is the primary key.
  */
+//@Entity(tableName = "venues", indices = arrayOf(Index("id")))
 @Entity(tableName = "venues")
 data class Venue(
-        @PrimaryKey @field:SerializedName("id") val id: String,
-        @field:SerializedName("name") val name: String?,
-        @field:SerializedName("address") val address: String?,
-        @field:SerializedName("distance") val distance: Int?,
-        @field:SerializedName("lng") val lng: Double?,
-        @field:SerializedName("lat") val lat: Double?
+        @PrimaryKey @field:SerializedName("id") var id: String ="",
+        @field:SerializedName("queried")  var queried: String ="",
+        @field:SerializedName("name")  var name: String ="",
+        @field:SerializedName("address") var address: String? ="",
+        @field:SerializedName("distance") var distance: Int? = 0,
+        @field:SerializedName("lng") var lng: Double? = 0.0,
+        @field:SerializedName("lat") var lat: Double? =0.0,
+        @Ignore var ignored: String? = null
 
 )
+
+//@Entity(tableName = "venues", indices = arrayOf(Index("query"), Index(value = *arrayOf("query"))))
+//class Venue {
+//
+//    @PrimaryKey
+//    @SerializedName("id")
+//    var id: String? = null
+//    @ColumnInfo(name = "query")
+//    @SerializedName("name")
+//    var name: String
+//    @SerializedName("address")
+//    var address: String? = null
+//    @SerializedName("distance")
+//    var distance: Int = 0
+//    @SerializedName("lng")
+//    var lng: Double = 0.toDouble()
+//    @SerializedName("lat")
+//    var lat: Double = 0.toDouble()
+//    @Ignore
+//    private val ignored: String? = null
+//}
+//
+//
+
